9fd3715472a7158a78a6116df98036f3ca006cea
abuhmam16 <bkrylearn3@gmail.com.com>
2023-10-19 00:27:57 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,23 +6,25 @@
  *
  * Return: a pointer to the resulting string 
  */
- char *_stract(char *dest, char *src)
+ char *_strcat(char *dest, char *src)
  {
- 	int i,j;
+ 	int i;
+	int j;
 
 	i=0;
-	j=0;
-
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
+	j = 0;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
 		i++;
 	}
+
 	dest[i] = '\0';
+
 	return (dest);
 }
